dcbf2b454535cbe350eaaf49d9cf34f89ffe3138
TJ Jiang <thaddeusjiang@gmail.com>
2019-09-28 11:26:28 +0900

    feat(register and login): init page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,109 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { navigate } from '@reach/router'
 
-const LoginPage: React.FC = () => <div>Login Page</div>
+const LoginPage: React.FC = () => {
+  const [accountMap, setAccountMap] = useState()
+
+  const accountMapStorage = window.localStorage.getItem('accountMap')
+
+  useEffect(() => {
+    if (accountMapStorage) {
+      setAccountMap(JSON.parse(accountMapStorage))
+    }
+  }, [setAccountMap, accountMapStorage])
+
+  const [username, setUsername] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [hasAccount, setHasAccount] = useState(false)
+  const [passwordError, setPasswordError] = useState(false)
+  const isExist = accountMap && accountMap.hasOwnProperty(username)
+
+  return (
+    <section>
+      {hasAccount ? (
+        <button onClick={() => setHasAccount(!hasAccount)}>Login Page</button>
+      ) : (
+        <button onClick={() => setHasAccount(!hasAccount)}>
+          Register Page
+        </button>
+      )}
+
+      <div>
+        {!hasAccount && isExist && '!'}
+        <label htmlFor="username">User Name: </label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      {!hasAccount && (
+        <div>
+          <label htmlFor="first_name">First Name: </label>
+          <input
+            type="text"
+            name="first_name"
+            id="first_name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </div>
+      )}
+
+      <div>
+        {passwordError && '!'}
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => {
+            setPasswordError(false)
+            setPassword(e.target.value)
+          }}
+        />
+      </div>
+      {hasAccount ? (
+        <button
+          onClick={() => {
+            const savedPassword = accountMap[username].password
+            console.debug({ savedPassword, password })
+            if (savedPassword === password) {
+              sessionStorage.setItem('loginBy', JSON.stringify({ username }))
+              navigate(`/${username}`)
+            } else {
+              setPasswordError(true)
+            }
+          }}
+        >
+          Login
+        </button>
+      ) : (
+        <button
+          disabled={
+            isExist || username === '' || firstName === '' || password === ''
+          }
+          onClick={() => {
+            const newAccountMap = {
+              ...accountMap,
+              [username]: { username, firstName, password },
+            }
+            localStorage.setItem('accountMap', JSON.stringify(newAccountMap))
+            sessionStorage.setItem('loginBy', username)
+
+            navigate(`/${username}`)
+          }}
+        >
+          Register
+        </button>
+      )}
+    </section>
+  )
+}
 
 export default LoginPage
--- src/pages/Tweets.tsx
+++ src/pages/Tweets.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
-const TweetsPage: React.FC = () => <div>TweetsPage</div>
+const TweetsPage: React.FC = () => {
+  const loginBy = sessionStorage.getItem('loginBy')
+  console.log(loginBy)
+  return (
+    <section>
+      <h1>Tweetes Page</h1>
+    </section>
+  )
+}
 
 export default TweetsPage
